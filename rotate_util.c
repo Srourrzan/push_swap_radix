7e952ebde5ef5ae341a6ad054fd57d4b1af3b1a9
Razan Srour <razanmsrour@gmail.com>
2025-02-16 12:48:01 +0300

    remove ascend and descend functions

--- rotate_util.c
+++ rotate_util.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rotate_operations.c                                :+:      :+:    :+:   */
+/*   rotate_util.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 11:35:36 by rsrour            #+#    #+#             */
-/*   Updated: 2025/02/13 13:23:46 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/02/16 12:45:10 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,98 +74,4 @@ void	rrr(t_list **root_a, t_list **root_b)
 {
 	reverse_rotate(root_a);
 	reverse_rotate(root_b);
-}
-
-int 	ft_ascend_reverse_rotate(t_list **a, t_list **b, int counter, int fd)
-{
-	t_list		*last_a;
-	t_list		*last_b;
-
-	last_a = *a;
-	if ((*b) != NULL)
-	{
-		last_b = *b;
-		while (last_b->next != NULL)
-			last_b = last_b->next;
-	}
-	while (last_a->next != NULL)
-		last_a = last_a->next;
-	while (last_a->content < (*a)->content)
-	{
-		if ((*b) && (last_b->content > (*b)->content))
-		{
-			rrr(a, b);
-			ft_putstr("rrr\n", fd);
-			counter++;
-			ft_display_stacks(a, b, fd);
-		}
-		else
-		{
-			reverse_rotate(a);
-			ft_putstr("rra\n", fd);
-			counter++;
-			ft_display_stacks(a, b, fd);
-		}
-		while (last_a->next != NULL)
-			last_a = last_a->next;
-		while ((*b) && last_b->next != NULL)
-			last_b = last_b->next;
-	}
-	return counter;
-}
-
-int 	ft_descend_reverse_rotate(t_list **a, t_list **b, int counter, int fd)
-{
-	t_list		*last_a;
-	t_list		*last_b;
-
-	last_b = *b;
-	if ((*a) != NULL)
-	{
-		last_a = *a;
-		while (last_a->next != NULL)
-			last_a = last_a->next;
-	}
-	while (last_b->next != NULL)
-		last_b = last_b->next;
-	printf("last b = %d\n", last_b->content);
-	if (last_b->content > (*b)->content)
-	{
-		printf("last_b %d > b %d = %d\n", last_b->content , (*b)->content, last_b->content > (*b)->content);
-		if (((*a) != NULL) && (last_a->content < (*a)->content))
-		{
-			rrr(a, b);
-			ft_putstr("rrr\n", fd);
-			counter++;
-			ft_display_stacks(a, b, fd);
-		}
-		else
-		{
-			reverse_rotate(b);
-			ft_putstr("rrb\n", fd);
-			counter++;
-			ft_display_stacks(a, b, fd);
-		}
-	}
-	return counter;
-}
-
-int		ft_dascend_rotate(t_list **a, t_list **b, int counter, int fd)
-{
-	t_list		*last_b;
-
-	last_b = *b;
-	if((*b))
-	{
-		while(last_b->next != NULL)
-			last_b = last_b->next;
-		if (last_b->content > (*b)->content)
-		{
-			rotate(b);
-			ft_putstr("rb\n", fd);
-			counter++;
-			ft_display_stacks(a, b, fd);
-		}
-	}
-	return counter;
 }
--- swap_util.c
+++ swap_util.c
@@ -6,7 +6,7 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 19:49:40 by rsrour            #+#    #+#             */
-/*   Updated: 2025/01/07 22:04:41 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/02/16 12:44:43 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,70 +39,4 @@ void	ss(t_list **root_a, t_list **root_b)
 {
 	swap(root_a);
 	swap(root_b);
-}
-
-int    ft_ascend_swap(t_list **a, t_list **b, int counter, int fd)
-{
-	t_status	*status;
-
-	status = malloc(sizeof(t_status));
-	if (!status)
-		return (counter);
-	status->status_1 = ((*a)->next != NULL) && ((*a)->content > (*a)->next->content);
-	status->status_2 = (*b) != NULL && ((*b)->next != NULL) && ((*b)->content < (*b)->next->content);
-    if(status->status_1)
-	{
-		if (status->status_1 && status->status_2)
-		{
-			ss(a, b);
-			ft_putstr("ss\n", fd);
-		}
-		else if (status->status_1)
-		{
-			swap(a);
-			ft_putstr("sa\n", fd);
-		}
-		else if (status->status_2)
-		{
-			swap(b);
-			ft_putstr("sb\n", fd);
-		}
-		counter++;
-		ft_display_stacks(a, b, fd);
-	}
-	free(status);
-	return counter;
-}
-
-int		ft_decscend_swap(t_list ** a, t_list **b, int counter, int fd)
-{
-	t_status	*status;
-
-	status = malloc(sizeof(status));
-	if (!status)
-		return (counter);
-	status->status_1 = ((*b)->next != NULL) && ((*b)->content < (*b)->next->content);
-	status->status_2 = ((*a) != NULL) && (*a)->next != NULL && ((*a)->content > (*a)->next->content);
-	if (status->status_1 || status->status_2)
-	{
-		if(status->status_1 && status->status_2)
-		{
-			ss(a, b);
-			ft_putstr("ss\n", fd);
-		}
-		else if (status->status_1 && !status->status_2)
-		{
-			swap(b);
-			ft_putstr("sb\n", fd);
-		}
-		else if (status->status_2)
-		{
-			swap(a);
-			ft_putstr("sa\n", fd);
-		}
-		counter++;
-		ft_display_stacks(a, b, fd);
-	}
-	free(status);
-	return counter;
 }
